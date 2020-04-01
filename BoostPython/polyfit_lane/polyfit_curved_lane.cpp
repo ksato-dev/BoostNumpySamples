3292b212e7adbd644fc8f2644b3cd04624a844f0
ksato-dev <k-sato@smartrobotics.jp>
2020-04-01 17:18:09 +0900

    Modify atai-watashi to sansho-watashi.

--- BoostPython/polyfit_lane/polyfit_curved_lane.cpp
+++ BoostPython/polyfit_lane/polyfit_curved_lane.cpp
@@ -5,10 +5,32 @@
 namespace p = boost::python;
 namespace np = boost::python::numpy;
 
-void polyfit(np::ndarray np_array)
+void extract_curved_points(const np::ndarray &curved_bin_image)
 {
-    int nd = np_array.get_nd();
+    int nd = curved_bin_image.get_nd();
+    std::cout << "__dim: " << nd << std::endl;
+
+    // return np::ndarray();
+}
+
+np::ndarray spline_fitting2(np::ndarray curved_points)
+{
+
+}
+
+np::ndarray polyfit(const np::ndarray &curved_bin_image)
+// np::ndarray polyfit(np::ndarray curved_bin_image)
+{
+    int nd = curved_bin_image.get_nd();
     std::cout << "dim: " << nd << std::endl;
+
+    // spline fitting (2 dim)
+
+    extract_curved_points(curved_bin_image);
+    
+    p::tuple shape = p::make_tuple(2);
+    np::dtype dtype = np::dtype::get_builtin<double>();
+    return np::zeros(shape, dtype);
 }
 
 BOOST_PYTHON_MODULE( polyfit_curved_lane ) {
